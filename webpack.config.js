c84cf4a79afb8d01a27799b6825f11dfe40f151d
Basil Goldman <basil@inqwise.com>
2021-12-02 10:49:49 +0200

    added dev dependencies, + scripts

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = function(env) {
+    const app = env.app;
+    return {
+        entry : "./src/index.js",
+        output: {
+            path: __dirname + "/public/js",
+            filename: "player.js"
+        },
+        optimization: {
+            minimize: app.conf === "prod",
+            minimizer: [new TerserPlugin({
+                parallel : 4,
+                extractComments: true,
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                        ecma: 2015
+                    }
+                }
+            })]
+        }
+    }
+}
